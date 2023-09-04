236247433daba0006b41cc2c5cba9d100c472053
RATCHANON CHUNSAKUNEE <kirigayannd6355@gmail.com>
2023-09-05 02:40:08 +0700

    maybe_completed

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -98,18 +98,39 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
+  const body = await request.json();
+
+  const parseResult = zStudentDleteBody.saeParse(body);
+  if(parseResult.success === false){
+    return NextResponse.json(
+      {
+        ok: false;
+        message: parseReesult.error.issues[0].message,
+      },
+      {status: 400}
+    );
+  }
 
   //check if student id exist
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === body.studntId
+  );
+  if(ffoundIndex === -1){
+    return NextResponse.json(
+      {ok:false, message: "Student Id does not exist"},
+      {status: 404}
+    );
+  }
 
   //perform removing student from DB. You can choose from 2 choices
   //1. use array filter method
-  // DB.students = DB.students.filter(...);
+  DB.students = DB.students.filter((std)=>std.studentId !== body.studentId);
 
   //or 2. use splice array method
   // DB.students.splice(...)
 
   return NextResponse.json({
     ok: true,
-    message: `Student Id xxx has been deleted`,
+    message: `Student Id ${body.studenId} has been deleted`,
   });
 };
